8e7e44df48e1279a16e7f5cf1558e55414c0b754
Bone4Coder <88817100+bone04@users.noreply.github.com>
2025-09-14 22:07:16 +0800

    Add files via upload
    upload app basic

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// app/page.tsx
+export default function Page() {
+return (
+  <h1 className="font-bold underline text-blue-500 bg-rose-200">
+      Hello world! App page.
+    </h1>
+
+)
+}
+
+
+// Pembayaran Ganti Uang Persediaan (GU) Pada Kantor Kecamatan Cina Kab. Bone Sumber Dana : DAU.
