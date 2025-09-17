16eefe36e3aee7a0a0c519414f86effd65681de8
Awish Dot <awishdotid@gmail.com>
2025-09-17 22:13:05 +0800

    917 20:12 shadcn form

--- app/user/add-user/page.tsx
+++ app/user/add-user/page.tsx
@@ -1,8 +1,13 @@
 import AddUserForm from "./add-user-form";
+
 export default function AddUserPage() {
 	return (
-		 
+			<>
+			<div>
+				<p>ini halaman add user</p>
+			</div>
 			<AddUserForm />
+			</>
 		
 
 	)
